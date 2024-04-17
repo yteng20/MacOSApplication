91ece5900794e5594faa0e7421ca1adf6c6d0194
yteng20 <yue.l.teng@gmail.com>
2024-04-17 00:16:34 -0600

    added video player

---
+++ newAPP/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  newAPP
+//
+//  Created by Yue Teng on 4/15/24.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
